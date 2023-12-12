fa91d8edbaa40e0ad0548edb4c7e7de957152a32
chenqiaoda <komorebi_cqd@163.com>
2023-12-12 23:17:12 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Button from '@mui/material/Button';
+const App = () => {
+  return (
+    <div >
+      <Button variant="contained">Hello world</Button>
+      <div className="w-12 h-12 bg-black">
+        header
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/ui/header.jsx
@@ -0,0 +1,20 @@
+
+import { css } from '@emotion/css'
+
+const Header = () => {
+    return (
+        <div className={css`
+    padding: 32px;
+    background-color: hotpink;
+    font-size: 24px;
+    border-radius: 4px;
+    &:hover {
+      color: white;
+    }
+  `}>
+            header
+        </div>
+    )
+}
+
+export default Header;
